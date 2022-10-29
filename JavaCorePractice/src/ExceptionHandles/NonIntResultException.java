cc65483f756933eff396194019f5e3173f1fcae8
Andrés K. Aquin <andruuus123@gmail.com>
2022-10-29 18:49:23 -0400

    Custom Exceptions

---
+++ JavaCorePractice/src/ExceptionHandles/NonIntResultException.java
@@ -0,0 +1,17 @@
+package ExceptionHandles;
+
+@SuppressWarnings("serial")
+public class NonIntResultException extends Exception{
+	int n;
+	int d;
+	
+	NonIntResultException(int i, int j) {
+		n = i;
+		d = j;
+	}
+	
+	public String toString() {
+		return "Result of " + n + " / " + d +
+				" is non-integer.";
+	}
+}
