377e303d872dee182122ab72282d9958f5e31e1f
Andrés K. Aquin <andruuus123@gmail.com>
2023-01-05 19:56:15 -0400

    indentation

--- JavaCorePractice/src/Arrays/ArrayApp.java
+++ JavaCorePractice/src/Arrays/ArrayApp.java
@@ -9,7 +9,7 @@ public class ArrayApp {
 		int nElems = 0; // number of items
 		int j; // loop counter
 		long searchKey; // key of item to search for
-		//--------------------------------------------------------------
+		// --------------------------------------------------------------
 		arr[0] = 77; // insert 10 items
 		arr[1] = 99;
 		arr[2] = 44;
@@ -21,30 +21,30 @@ public class ArrayApp {
 		arr[8] = 66;
 		arr[9] = 33;
 		nElems = 10; // now 10 items in array
-		//--------------------------------------------------------------
-		for(j=0; j<nElems; j++) // display items
-		System.out.print(arr[j] + " ");
+		// --------------------------------------------------------------
+		for (j = 0; j < nElems; j++) // display items
+			System.out.print(arr[j] + " ");
 		System.out.println("");
-		//--------------------------------------------------------------
+		// --------------------------------------------------------------
 		searchKey = 66; // find item with key 66
-		for(j=0; j<nElems; j++) // for each element,
-		if(arr[j] == searchKey) // found item?
-		break; // yes, exit before end
-		if(j == nElems) // at the end?
-		System.out.println("Can’t find " + searchKey); // yes
+		for (j = 0; j < nElems; j++) // for each element,
+			if (arr[j] == searchKey) // found item?
+				break; // yes, exit before end
+		if (j == nElems) // at the end?
+			System.out.println("Can’t find " + searchKey); // yes
 		else
-		System.out.println("Found " + searchKey); // no
-		//--------------------------------------------------------------
+			System.out.println("Found " + searchKey); // no
+		// --------------------------------------------------------------
 		searchKey = 55; // delete item with key 55
-		for(j=0; j<nElems; j++) // look for it
-		if(arr[j] == searchKey)
-		break;
-		for(int k=j; k<nElems-1; k++) // move higher ones down
-		arr[k] = arr[k+1];
+		for (j = 0; j < nElems; j++) // look for it
+			if (arr[j] == searchKey)
+				break;
+		for (int k = j; k < nElems - 1; k++) // move higher ones down
+			arr[k] = arr[k + 1];
 		nElems--; // decrement size
-		//--------------------------------------------------------------
-		for(j=0; j<nElems; j++) // display items
-		System.out.print( arr[j] + " ");
+		// --------------------------------------------------------------
+		for (j = 0; j < nElems; j++) // display items
+			System.out.print(arr[j] + " ");
 		System.out.println("");
 	}
 }
